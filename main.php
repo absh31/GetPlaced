dd600ff3b39c2726f4337f0fc760e35f82b1e84b
Dharmit Shah <81405914+dharmit01@users.noreply.github.com>
2021-06-28 19:25:26 +0530

    Add files via upload

---
+++ main.php
@@ -0,0 +1,82 @@
+<?php include "header.php" ?>
+
+<div class="container">
+       <div class="row">
+            <div class="box">
+                <div class="full-width-image">
+                    <img src="Group10.png" class="img-fluid" alt="Image">
+                </div>
+                <br>
+                <div class="text">
+                    <img src="Logo(GP)-02.png" alt="Logo" class="img-fluid">
+                    <h1>Where Talent Gets Opportunities</h1>
+                </div>
+            </div>
+       </div> 
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="about">
+                <h2>WHAT ARE WE</h2>
+                <br>
+                <h4>We provide a platform for the students and job seekers</h4>
+                <h4>to get placed into certified companies.</h4>
+                <br>
+                <h4>Either you can enroll as a College Student or</h4>
+                <h4>an individual seeking for job.</h4>
+            </div>
+            
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="facts">
+                <h2>SOME FACTS ABOUT US</h2>
+            </div>
+            <br>
+            <div class="col-lg-3 mb-4 sm-6">
+                <div class="card border border-dark">
+                    <div class="card-body">
+                        <h5 class="card-title">9600+</h5>
+                        <p class="card-text">
+                            Our Users
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-4 sm-6">
+                <div class="card border border-dark">
+                    <div class="card-body">
+                        <h5 class="card-title">50+</h5>
+                        <p class="card-text">
+                           Registered Users
+                        </p>      
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-4 sm-6">
+                <div class="card border border-dark">
+                    <div class="card-body">
+                        <h5 class="card-title">300+</h5>
+                        <p class="card-text">
+                           Registered Companies
+                        </p>      
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mb-4 sm-6">
+                <div class="card border border-dark">
+                    <div class="card-body">
+                        <h5 class="card-title">2500+</h5>
+                        <p class="card-text">
+                           Placed Users
+                        </p>      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include "footer.php" ?>
