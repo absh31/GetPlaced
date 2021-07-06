47b84e57468a857181b118ca88f46601d6b26a18
Dharmit Shah <81405914+dharmit01@users.noreply.github.com>
2021-07-06 15:15:32 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,75 @@
+    <!-- Footer -->
+<footer class="text-center text-lg-start text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-1 ">
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5" style="color:white">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <p style="font-size:16pt">
+            Nr. Visat Cross Roads,<br>
+            Sabarmati-Koba Highway,<br>
+            Chandkheda,<br>
+            Ahmedabad - 382424
+          </p>
+          <p style="font-size:16pt"><i class="fa fa-phone me-3"></i>+91-1234567890</p>
+          <p style="font-size:16pt"><i class="fa fa-envelope me-3"></i>query@getplaced.com</p>
+          
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <img src="main_logo_light.png" alt="logo" style="width:300px">
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-2 mx-auto mb-md-0 mb-4" style="font-size:16pt;">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4" style="font-size:16pt">
+            Login Links
+          </h6>
+          <p style="font-size:16pt"><a class="text-reset" style="text-decoration:none;" href="#">Employer Login</a></p>
+          <p style="font-size:16pt"><a class="text-reset" style="text-decoration:none;" href="#">Student Login</a></p>
+          <p style="font-size:16pt"><a class="text-reset" style="text-decoration:none;" href="#">University Login</a></p>
+          
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05); color:white">
+    © 2021 Copyright: FRIENDS
+  </div>
+  
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+ 
+ <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
